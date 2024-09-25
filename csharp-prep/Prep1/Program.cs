470670a7148a3adcc8acf84ed67ac291fac92aa2
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-09-25 19:00:09 +0100

    Updated to camelcase convention

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string f_name = Console.ReadLine();
+        string FName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string l_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
+        string LName = Console.ReadLine();
+        Console.WriteLine($"Your name is {LName}, {FName} {LName}.");
         
     }
 }
