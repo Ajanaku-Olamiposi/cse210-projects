b66f2192e2c0a61a089e84df8c81e013c897f3d9
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-09-26 01:30:56 +0100

    Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int score = int.Parse(gradePercentage);
+
+        string grade;
+        if(score >= 90)
+        {
+            grade = "A";
+        }
+        else if(score >= 80)
+        {
+            grade = "B";
+        }
+        else if(score >= 70)
+        {
+            grade = "C";
+        }
+        else if(score >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        string message;
+        if(score >= 70)
+        {
+            message = "Congratulations, you passed the course.";
+        }
+        else
+        {
+            message = "You failed the course but not in life, please be encouraged to retake the course and study harder.";
+        }
+        
+        string sign = "";
+        if(score >= 60 && score <= 92)
+        {
+            if((score % 10) < 3)
+            {
+                sign = "-";
+            } 
+            else if((score % 10) >= 7)
+            {
+                sign = "+";
+            } 
+        }
+        Console.WriteLine($"Your grade is {grade}{sign}. {message}");
     }
 }
