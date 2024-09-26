efb9c7737e1467be36d75939caf1a4ca8d28acba
Ajanaku-Olamiposi <olamiposajanaku53@gmail.com>
2024-09-26 11:00:08 +0100

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        int guessCount = 0;
+        string message;
+        string ans = "yes";
+
+        while (ans.ToLower() == "yes")
+        {
+            guessCount = 0;
+            while (guess != num)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount ++;
+                if (guess < num)
+                {
+                    message = "Higher";
+                }
+                else if (guess > num)
+                {
+                    message = "Lower";
+                }
+                else
+                {
+                    message = $"You guessed it!. It took you {guessCount}tries. ";
+                }
+                Console.WriteLine(message);
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            ans = Console.ReadLine();
+            if (ans.ToLower() == "yes")
+            {
+                num = randomGenerator.Next(1, 100);
+                guess = -1;
+            }
+        }
     }
 }
